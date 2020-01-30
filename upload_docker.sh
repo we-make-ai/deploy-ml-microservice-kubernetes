197d405df881668654b6872843002c0a871f33be
B.Mayr <b.mayr@we-make.ai>
2020-01-30 19:27:26 +0100

    ...

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="ulat/ml_microservice"
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag mlapi $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
