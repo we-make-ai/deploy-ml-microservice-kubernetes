cf073754bdd1edaef9b2329878c1e683cc79009a
ulat <6483693+ulat@users.noreply.github.com>
2020-01-31 11:16:35 +0100

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="ulat/ml_microservice"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mlmicroserviceapi\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=mlmicroserviceapi
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mlmicroserviceapi 8000:80
